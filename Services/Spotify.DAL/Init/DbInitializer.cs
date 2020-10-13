865b652426be89562b42e95eb07ad1406aa22496
maksim <maks_10000@bk.ru>
2020-10-13 19:36:59 +0300

    Филлер БД

---
+++ Services/Spotify.DAL/Init/DbInitializer.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Spotify.Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spotify.DAL.Init
+{
+    public class DbInitializer
+    {
+        private readonly SpotifyDbContext _db;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ILogger<DbInitializer> _logger;
+
+        public DbInitializer(SpotifyDbContext db, UserManager<User> userManager, RoleManager<IdentityRole> roleManager,
+            ILogger<DbInitializer> logger)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _logger = logger;
+        }
+
+        public void Init()
+        {
+            var db = _db.Database;
+
+            try
+            {
+                _logger.LogInformation("Проведение миграций БД");
+                db.Migrate();
+
+                _logger.LogInformation("Добавление обощённых тегов");
+                InitializeTagFamilies();
+
+                _logger.LogInformation("Добавление тегов");
+                InitializeTags();
+
+                _logger.LogInformation("Добавление жанров");
+                InitializeGenre();
+
+                _logger.LogInformation("Добавление пользователей");
+                InitializeUsers();
+
+                _logger.LogInformation("Добавление авторов");
+                InitializeAuthors();
+
+
+                _logger.LogInformation("Заполнение БД треками");
+                InitializeTracks();
+            }
+            catch(Exception ex)
+            {
+                _logger.LogCritical(new EventId(-1), ex, "Ошибка инициализации БД");
+                
+                throw;
+            }
+        }
+        private void InitializeTagFamilies()
+        {
+
+        }
+        private void InitializeTags()
+        {
+
+        }
+        private void InitializeGenre()
+        {
+
+        }
+
+        private void InitializeUsers()
+        {
+            
+
+        }
+        private void InitializeTracks()
+        {
+
+        }
+
+
+        private void InitializeAuthors()
+        {
+
+        }
+
+
+    }
+}
---
+++ Services/Spotify.DAL/Init/TestData.cs
@@ -0,0 +1,44 @@
+﻿using Spotify.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spotify.DAL.Init
+{
+    public static class TestData
+    {
+        public static TagFamily[] TagFamilies { get; } = new[]
+        {
+            new TagFamily { Id = 1, Name = "Жизнерадостная музыка"},
+            new TagFamily { Id = 2, Name = "Быстрая музыка"},
+            new TagFamily { Id = 3, Name = "Медленная музыка"},
+            new TagFamily { Id = 4, Name = "Смешанная музыка"},
+        };
+
+        public static TrackTag[] Tags { get; } = new[]
+        {
+            new TrackTag { Id = 1, Name = "Весёлая", TagFamily = new[] { TagFamilies[0], TagFamilies[1] } },
+            new TrackTag { Id = 2, Name = "Грустная", TagFamily = new[] { TagFamilies[2] } },
+            new TrackTag { Id = 3, Name = "Рок", TagFamily = new[] { TagFamilies[3] } }
+        };
+
+        public static Genre[] Genres { get; } = new[]
+        {
+            new Genre { Id = 1, Name = "Рок", Description = "Рок музыка (англ. Rock music)", Plays = 0 },
+            new Genre { Id = 2, Name = "Рэп", Description = "Рэп (англ. rap, rapping)", Plays = 0 },
+            new Genre { Id = 3, Name = "Альтернативная музыка", Description = "Жанр рок-музыки, сформировавшийся из музыкального андеграунда)", Plays = 0 },
+            new Genre { Id = 4, Name = "Фонк", Description = "Поджанр хип-хопа и трэпа, вдохновлённый Мемфис-рэпом 1990-х годов.", Plays = 0 },
+            new Genre { Id = 5, Name = "Хип-хоп", Description = "Музыкальный жанр, сочетающий рэп с электронным битом, сэмплами и прочим музыкальным сопровождением в исполнении ди-джея.", Plays = 0 }
+        };
+
+        public static Author[] Authors { get; } = new[]
+        {
+            new Author { Id = 1, Name = "Подзалупник", }
+        };
+
+        public static string[] Roles = new[]
+        {
+            "Admin", "User", "Moderator"
+        };
+    }
+}
