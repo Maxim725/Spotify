36a53a2dcfe641ddc6dc927da55db8988dfaa306
Nikita Kruglov <n.kruglov@hotmail.com>
2020-10-30 12:15:14 +0300

    Обвновление моделей

--- UI/Spotify/Program.cs
+++ UI/Spotify/Program.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Spotify.Data;
 
 namespace Spotify
 {
@@ -15,10 +17,32 @@ namespace Spotify
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = CreateHostBuilder(args).Build();
+            CreateDbIfNotExists(host);
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+		public static void CreateDbIfNotExists(IHost host)
+		{
+			using (IServiceScope scope = host.Services.CreateScope())
+			{
+				IServiceProvider services = scope.ServiceProvider;
+
+				try
+				{
+					var context = services.GetRequiredService<ApplicationDbContext>();
+					context.Database.EnsureCreated();
+					DbInitializer.Initialize(context);
+				}
+				catch (Exception ex)
+				{
+					var logger = services.GetRequiredService<ILogger<Program>>();
+					logger.LogError(ex, "An error occurred creating the DB.");
+				}
+			}
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
