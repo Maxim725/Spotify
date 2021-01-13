1d8bd282dda3e1ce3fd1cd3d8ce62dbac6eb1617
maksim <maks_10000@bk.ru>
2021-01-13 14:00:12 +0300

    НЕ алгоритм рекомендаций

--- Services/Spotify.Services/SelectionService.cs
+++ Services/Spotify.Services/SelectionService.cs
@@ -6,6 +6,9 @@ using Spotify.Domain.Entities;
 using Spotify.DAL;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Collections;
+using Spotify.Domain.SpecialModels;
 
 namespace Spotify.Services
 {
@@ -100,7 +103,82 @@ namespace Spotify.Services
             return _db.Tracks.Include(x =>x.Album).FirstOrDefault(x => x.TrackId.Equals(trackId)).Album;
         }
 
+
         // систему рекомендаций сюда дописать
+        public IEnumerable<Track> Recommendation(IEnumerable<int> tracksId)
+        {
+            //// Брать срез максимум из 50-ти треков
+            //List<int> sliceTracksId = new List<int>();
+            //for(int i = 0; i< tracksId.Count(); i++)
+            //{
+            //    if (i.Equals(50)) break;
+            //    sliceTracksId.Add(tracksId.ElementAt(i));
+            //}
+
+            //// Массив самых популярных тегов (тег + количество повторений)
+            //IEnumerable<int> trackTags = _db.TrackTag.Where(x => sliceTracksId.Contains(x.TrackId)).Select(x => x.TagId).ToList();
+            //// var tagRepetitions = trackTags.GroupBy(x => x).Select(x =>  new { Id = x, CountId = x.Count() }).OrderBy(x => x.CountId);
+
+            //// Массив массив обобщённых тегов (тег + количество повторений)
+
+            //var templateTrackTags = _db.Tags.Where(x => trackTags.Contains(x.TagId)).GroupBy(x => x.FamilyId).Select(x => new { Id = x, CountId = x.Count() }).OrderBy(x => x.CountId).ToList();
+            ////if (templateTrackTags.Count() > 3)
+            ////    templateTrackTags = templateTrackTags.Take(3);
+
+
+
+            //var tracks = new List<RecommendationModel>();
+            //if (templateTrackTags.Count() == 0)
+            //    return null;
+
+            //var id = templateTrackTags.ElementAt(0).Id;
+
+            //var trackTagsId = _db.Tags.Where(x => x.FamilyId.Equals(id)).Select(x=> x.TagId);
+
+            //var recTracks = _db.TrackTag.Where(x => trackTagsId.Contains(x.TagId)).GroupBy(x => x.TrackId);
+
+            //foreach(var track in recTracks)
+            //{
+            //    var idTrack = track.Key;
+            //    int w = 0;
+            //    foreach(var repT in track) {
+            //        if (trackTagsId.Contains(repT.TagId)) w++;
+            //    }
+            //    tracks.Add(new RecommendationModel(idTrack, w));
+            //}
+            //tracks.OrderBy(x => x.Weight);
+
+
+            //var result = _db.Tracks.Where(t => tracks/*.Take(50)*/.Select(x => x.Id).Contains(t.TrackId));
+            //if (result.Count() > 50)
+            //    result = result.Take(50);
+
+            // Возникло множество проблем, надо будет алгоритм пилить (я не сильён в рекомендательных алгоритмах)
+            var rand = new Random();
+            int[] randId = new int[50];
+            int countTracks = _db.Tracks.Count();
+            if(countTracks > 50)
+            {
+                for(int i = 0; i < randId.Length; i++)
+                {
+                    int value = rand.Next(1, countTracks - 1);
+                    if (!randId.Contains(value))
+                    randId[i] = value;
+                }
+                
+            }
+            else
+            {
+                for (int i = 0; i < randId.Length; i++)
+                {
+                    int value = rand.Next(1, countTracks - 1);
+                    randId[i] = value;
+                }
+            }
+            var result = _db.Tracks.Where(x => randId.Contains(x.TrackId));
+
+            return result;
+        }
 
 
     }
